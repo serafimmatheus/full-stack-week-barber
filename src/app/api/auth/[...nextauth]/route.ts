cccb36f5fcf417ca3067685569c62a1d995d8e82
matheusserafimm <matheus.serafim.t_mentores@grupoboticario.com.br>
2024-04-15 08:24:29 -0300

    feat: add AuthProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,16 @@
+import { db } from '@/app/_lib/prisma'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { db } from '@/app/_lib/prisma'
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   adapter: PrismaAdapter(db),
-}
+})
 
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST }
